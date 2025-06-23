f7fad072c8785497916c0042872472a4d7f7a15a
Zacklack <contact@zacklack.de>
2025-06-23 23:59:27 +0200

    Add application assets for Demokratie Quiz

--- applications/frontend/src/components/Layout.tsx
+++ applications/frontend/src/components/Layout.tsx
@@ -20,6 +20,7 @@ export default function Layout({
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
+        <meta name="apple-mobile-web-app-title" content="Quiz" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
