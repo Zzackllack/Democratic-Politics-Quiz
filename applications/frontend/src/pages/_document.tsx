24665188d2ac26ae0a0d573c491e9c6d024de1f0
Zacklack <zzacklack@gmail.com>
2025-06-24 00:29:07 +0200

    feat: add PWA support and install hint

--- applications/frontend/src/pages/_document.tsx
+++ applications/frontend/src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#cfcfcf" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
